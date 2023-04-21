bfd2e26931141ee0550363c3a6c7a280e6c8cdbc
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-21 19:48:58 -0300

    Small changes

--- modules/calculator.js
+++ modules/calculator.js
@@ -1,7 +1,5 @@
 const calculator = () => {
-  const add = (a, b) => {
-    return a + b;
-  };
+  const add = (a, b) => a + b;
   const subtract = (a, b) => a - b;
   const divide = (a, b) => a / b;
   const multiply = (a, b) => a * b;

0a25b76fc7c17ecdb3a8cc08f45e4d037ab48e2e
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-21 17:18:28 -0300

    Add test for subtract

--- modules/calculator.test.js
+++ modules/calculator.test.js
@@ -12,3 +12,15 @@ it('add(10 + 123)', () => {
 it('add(10 + (-100))', () => {
   expect(calculator().add(10, -100)).toBe(-90);
 });
+
+it('subtract(3 - 2)', () => {
+  expect(calculator().subtract(3, 2)).toBe(1);
+});
+
+it('subtract(10 - 123)', () => {
+  expect(calculator().subtract(10, 123)).toBe(-113);
+});
+
+it('subtract(10 - (-100))', () => {
+  expect(calculator().subtract(10, -100)).toBe(110);
+});

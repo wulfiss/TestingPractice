ec5748a52c701f898133fcf9b92605e78199c888
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-21 16:51:43 -0300

    add tests for reversestring

--- modules/reverseString.test.js
+++ modules/reverseString.test.js
@@ -1,1 +1,13 @@
 import reverseString from './reverseString.js';
+
+it('one word', () => {
+  expect(reverseString('Hello').toBe('olleH'));
+});
+
+it('two words', () => {
+  expect(reverseString('Hello World').toBe('olleH dlroW'));
+});
+
+it('unreverse', () => {
+  expect(reverseString('gniR eht mrala').toBe('Ring the alarm'));
+});

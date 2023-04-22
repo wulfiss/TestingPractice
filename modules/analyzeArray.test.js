eeb38e82c43109135fade3cb032ba328bc615056
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-22 18:47:14 -0300

    Add max test

--- modules/analyzeArray.test.js
+++ modules/analyzeArray.test.js
@@ -1,13 +1,13 @@
 import { analyzeArray } from './analyzeArray.js';
 
-it('average && min', () => {
-  expect(analyzeArray([2, 2, 2])).toStrictEqual({ average: 2, min: 2 });
+it('average && min && max', () => {
+  expect(analyzeArray([2, 2, 2])).toStrictEqual({ average: 2, min: 2, max: 2 });
 });
 
-it('average && min', () => {
-  expect(analyzeArray([2, 3, 1, 5, 6])).toStrictEqual({ average: 3.4, min: 1 });
+it('average && min && max', () => {
+  expect(analyzeArray([2, 3, 1, 5, 6])).toStrictEqual({ average: 3.4, min: 1, max: 6 });
 });
 
-it('average && min with negative', () => {
-  expect(analyzeArray([2, 3, 1, -5, 6, -10])).toStrictEqual({ average: -0.5, min: -10 });
+it('average && min && max with negative', () => {
+  expect(analyzeArray([2, 3, 1, -5, 6, -10])).toStrictEqual({ average: -0.5, min: -10, max: 6 });
 });

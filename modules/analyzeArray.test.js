1336062341af9f7a5e4ca6f82d5ee80eae41ffb4
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-22 18:45:02 -0300

    Add test with negative values

--- modules/analyzeArray.test.js
+++ modules/analyzeArray.test.js
@@ -7,3 +7,7 @@ it('average && min', () => {
 it('average && min', () => {
   expect(analyzeArray([2, 3, 1, 5, 6])).toStrictEqual({ average: 3.4, min: 1 });
 });
+
+it('average && min with negative', () => {
+  expect(analyzeArray([2, 3, 1, -5, 6, -10])).toStrictEqual({ average: -0.5, min: -10 });
+});

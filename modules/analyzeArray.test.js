dacdd9f0dc431896da41aa49e910bc1fc8e88cdd
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-22 18:40:31 -0300

    Add min test

--- modules/analyzeArray.test.js
+++ modules/analyzeArray.test.js
@@ -1,9 +1,9 @@
 import { analyzeArray } from './analyzeArray.js';
 
-it('average', () => {
-  expect(analyzeArray([2, 2, 2])).toStrictEqual({ average: 2 });
+it('average && min', () => {
+  expect(analyzeArray([2, 2, 2])).toStrictEqual({ average: 2, min: 2 });
 });
 
-it('average', () => {
-  expect(analyzeArray([2, 3, 1, 5, 6])).toStrictEqual({ average: 3.4 });
+it('average && min', () => {
+  expect(analyzeArray([2, 3, 1, 5, 6])).toStrictEqual({ average: 3.4, min: 1 });
 });

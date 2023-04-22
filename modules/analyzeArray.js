7df4dd22b7dc3cd0e61a28c24a02bb58170bb7a9
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-22 18:38:51 -0300

    Add average

--- modules/analyzeArray.js
+++ modules/analyzeArray.js
@@ -1,3 +1,12 @@
-const analyzeArray = (arr) => {};
+const analyzeArray = (arr) => {
+  const averageCal = () => {
+    let sum = arr.reduce((accumulator, current) => accumulator + current, 0);
+    return sum / arr.length;
+  };
+
+  return {
+    average: averageCal(),
+  };
+};
 
 export { analyzeArray };

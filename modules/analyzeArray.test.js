b9f2376b02bac4e8a217402dca71fa3d571f4d84
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-22 18:52:11 -0300

    Add test for length

--- modules/analyzeArray.test.js
+++ modules/analyzeArray.test.js
@@ -1,13 +1,13 @@
 import { analyzeArray } from './analyzeArray.js';
 
-it('average && min && max', () => {
-  expect(analyzeArray([2, 2, 2])).toStrictEqual({ average: 2, min: 2, max: 2 });
+it('average && min && max && length', () => {
+  expect(analyzeArray([2, 2, 2])).toStrictEqual({ average: 2, min: 2, max: 2, length: 3 });
 });
 
-it('average && min && max', () => {
-  expect(analyzeArray([2, 3, 1, 5, 6])).toStrictEqual({ average: 3.4, min: 1, max: 6 });
+it('average && min && max && length', () => {
+  expect(analyzeArray([2, 3, 1, 5, 6])).toStrictEqual({ average: 3.4, min: 1, max: 6, length: 5 });
 });
 
-it('average && min && max with negative', () => {
-  expect(analyzeArray([2, 3, 1, -5, 6, -10])).toStrictEqual({ average: -0.5, min: -10, max: 6 });
+it('average && min && max && length with negative', () => {
+  expect(analyzeArray([2, 3, 1, -5, 6, -10])).toStrictEqual({ average: -0.5, min: -10, max: 6, length: 6 });
 });

cc453bb831d5101a656c7fb77928d72efc5b0332
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-22 16:45:21 -0300

    add test for function satellite

--- modules/caesarCipher.test.js
+++ modules/caesarCipher.test.js
@@ -1,13 +1,23 @@
 import caesarCipher from './caesarCipher.js';
+import { shiftChar } from './caesarCipher.js';
 
-it('shift 5', () => {
-  expect(caesarCipher('Hello world', 5)).toBe('mjqqt btwqi');
+/* it('shift 5', () => {
+  expect(caesarCipher('He', 5)).toBe('mjqqt btwqi');
 });
-
-it('shift 0', () => {
+ */
+/* it('shift 0', () => {
   expect(caesarCipher('Hello world', 0)).toBe('Hello world');
 });
 
 it('test punctuation', () => {
   expect(caesarCipher('Hello!, World!!!', 10)).toBe('rovvy!, gybvn!!!');
+});
+ */
+
+it('H', () => {
+  expect(shiftChar(72, 5, 90, 65)).toBe(77);
+});
+
+it('e', () => {
+  expect(shiftChar(101, 5, 122, 92)).toBe(106);
 });
